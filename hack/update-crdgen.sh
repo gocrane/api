24a058aaca54288992c0fe98e78f830239fd5e1a
Jesse Meng <mengfj@gmail.com>
2021-12-10 13:58:09 +0800

    make all with controller-gen 0.7.0

--- hack/update-crdgen.sh
+++ hack/update-crdgen.sh
@@ -5,7 +5,7 @@ set -o nounset
 set -o pipefail
 
 CONTROLLER_GEN_PKG="sigs.k8s.io/controller-tools/cmd/controller-gen"
-CONTROLLER_GEN_VER="v0.5.0"
+CONTROLLER_GEN_VER="v0.7.0"
 
 source hack/util.sh
 
