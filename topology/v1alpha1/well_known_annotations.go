0be487b3308a4d435204070aa065c26dc9baeb42
Garrybest <garrybest@foxmail.com>
2022-08-08 11:45:14 +0800

    add well know labels and annotations
    Signed-off-by: Garrybest <garrybest@foxmail.com>

---
+++ topology/v1alpha1/well_known_annotations.go
@@ -0,0 +1,30 @@
+package v1alpha1
+
+const (
+	// AnnotationPodCPUPolicyKey is the pod annotation key of cpu-policy.
+	AnnotationPodCPUPolicyKey = "topology.crane.io/cpu-policy"
+
+	// AnnotationPodTopologyAwarenessKey is the pod annotation key of topology-awareness.
+	AnnotationPodTopologyAwarenessKey = "topology.crane.io/topology-awareness"
+
+	// AnnotationPodTopologyResultKey is the pod scheduling annotation key of topology-result.
+	AnnotationPodTopologyResultKey = "topology.crane.io/topology-result"
+)
+
+const (
+	// AnnotationPodCPUPolicyNone specifies none cpu policy. If specified, pod
+	// will use the default CPUSet.
+	AnnotationPodCPUPolicyNone = "none"
+
+	// AnnotationPodCPUPolicyExclusive specifies exclusive cpu policy.  If specified,
+	// pod will never share CPUSet with others.
+	AnnotationPodCPUPolicyExclusive = "exclusive"
+
+	// AnnotationPodCPUPolicyNUMA specifies NUMA cpu policy. If specified, pod
+	// will use the default CPUSet which belongs to single NUMA node.
+	AnnotationPodCPUPolicyNUMA = "numa"
+
+	// AnnotationPodCPUPolicyImmovable specifies immovable cpu policy. If specified,
+	// pod will use part of the default CPUSet to avoid uncertain context switch.
+	AnnotationPodCPUPolicyImmovable = "immovable"
+)
