a66876c72c4038117444ff8111c35f8a45383321
mango <xu.weiKyrie@foxmail.com>
2022-12-08 15:41:25 +0800

    chore: move recommend struct from kubectl-crane to api

--- analysis/v1alpha1/types.go
+++ analysis/v1alpha1/types.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	autoscalingapi "github.com/gocrane/api/autoscaling/v1alpha1"
+	autoscalingv2 "k8s.io/api/autoscaling/v2beta2"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -345,3 +347,37 @@ type RecommendationRuleList struct {
 
 	Items []RecommendationRule `json:"items"`
 }
+
+// ProposedRecommendation is the result for one recommendation
+type ProposedRecommendation struct {
+	// EffectiveHPA is the proposed recommendation for type Replicas
+	EffectiveHPA *EffectiveHorizontalPodAutoscalerRecommendation `json:"effectiveHPA,omitempty"`
+
+	// ReplicasRecommendation is the proposed replicas for type Replicas
+	ReplicasRecommendation *ReplicasRecommendation `json:"replicasRecommendation,omitempty"`
+
+	// ResourceRequest is the proposed recommendation for type Resource
+	ResourceRequest *ResourceRequestRecommendation `json:"resourceRequest,omitempty"`
+}
+
+type ReplicasRecommendation struct {
+	Replicas *int32 `json:"replicas,omitempty"`
+}
+
+type EffectiveHorizontalPodAutoscalerRecommendation struct {
+	MinReplicas *int32                     `json:"minReplicas,omitempty"`
+	MaxReplicas *int32                     `json:"maxReplicas,omitempty"`
+	Metrics     []autoscalingv2.MetricSpec `json:"metrics,omitempty"`
+	Prediction  *autoscalingapi.Prediction `json:"prediction,omitempty"`
+}
+
+type ResourceRequestRecommendation struct {
+	Containers []ContainerRecommendation `json:"containers,omitempty"`
+}
+
+type ContainerRecommendation struct {
+	ContainerName string       `json:"containerName,omitempty"`
+	Target        ResourceList `json:"target,omitempty"`
+}
+
+type ResourceList map[corev1.ResourceName]string
