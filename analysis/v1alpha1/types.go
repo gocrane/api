fa1964ee749e518e4341d18c52f95058ef6d6bd0
whitebear009 <whitebear009@163.com>
2022-12-13 22:03:36 +0800

    add config for RecommendationRule

--- analysis/v1alpha1/types.go
+++ analysis/v1alpha1/types.go
@@ -331,6 +331,9 @@ type Recommender struct {
 
 	// Recommender's Name
 	Name string `json:"name"`
+	// Override Recommendation configs
+	// +optional
+	Config map[string]string `json:"config,omitempty"`
 }
 
 // NamespaceSelector describes how to select namespaces for recommend
